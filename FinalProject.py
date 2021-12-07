540b0f12598327bb777cc8de441a5b1e1eb33603
Kifayat U. Khan <Kkhan4072@conestogac.on.ca>
2021-12-07 03:11:58 -0500

    Expanded the Comments

--- FinalProject.py
+++ FinalProject.py
@@ -5,13 +5,49 @@
 #              Weapons can kills specific enemies, upon selecting the right weapon , the user kills the enemy,
 #              and by killing the enemy the user receives points.
 
+
+# Levels
+#   Level 1 -> 0-10 points
+#   Level 2 -> 10-20
+#   level 3 -> 30-40
+
 # Class for Weapons
+#   Stick 
+#       Level 1
+#       strenght -> 2
+#   Sword 
+#       Level 2
+#       strenght -> 6
+#   Crossbow 
+#       Level 3
+#       strenght -> 8
+
 # Class for Enemies
-# Display Weapons
+#   Snake 
+#       Killed by Stick, Sword
+#       Points -> 10
+#   Hyena 
+#       Killed by Crossbow, Sword
+#       Points -> 10
+#   Bear 
+#       Killed by Crossbow
+#       Points -> 10
+
+# Display Weapons Available
+#   If Points
+#       Points 0-10 -> Stick
+#       Points 10-20 -> Sword
+#       Points 20-30 -> Crossbow
 # Input for Selection
+
+
 # Display Enemies
+#   Snake
+#   Hyena
+#   Bear
 # Select an Enemy
-# Conditional Statments 
+
+# Conditional Statments
 #   If weapon matches -> kill -> get points
 #   else -> fail -> no points
 # Store the points in an external file
