c69585aac3be3c1196526a329d02044c153b67a5
Kifayat U. Khan <Kkhan4072@conestogac.on.ca>
2021-12-07 02:55:57 -0500

    Added Pointer comments for the code

--- FinalProject.py
+++ FinalProject.py
@@ -5,3 +5,13 @@
 #              Weapons can kills specific enemies, upon selecting the right weapon , the user kills the enemy,
 #              and by killing the enemy the user receives points.
 
+# Class for Weapons
+# Class for Enemies
+# Display Weapons
+# Input for Selection
+# Display Enemies
+# Select an Enemy
+# Conditional Statments 
+#   If weapon matches -> kill -> get points
+#   else -> fail -> no points
+# Store the points in an external file
